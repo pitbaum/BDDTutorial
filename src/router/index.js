aba4512ceca8ec456e20290778f51e50d96c0eb2
pitbaum <72408661+pitbaum@users.noreply.github.com>
2021-12-14 11:35:40 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import Dashboard from "../views/Dashboard.vue";
-import Login from "../views/Login.vue";
 
 const routes = [
-  {
-    path: "/login",
-    name: "Login",
-    component: Login,
-  },
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    component: Dashboard,
-  },
+
 ];
 
 const router = createRouter({
